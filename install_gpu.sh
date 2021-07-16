b2b8a67a9a2266880e05e50cb1b47f658781abfa
Sourav Dutta <iitkgp.sourav@gmail.com>
2021-07-16 10:59:33 -0500

    SD::Updates gpu install script with correct tag

--- install_gpu.sh
+++ install_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A bash script to install PyNIROM with GPU support
 
-git clone --depth 1 --branch 6113f10 git@github.com:titu1994/tfdiffeq.git
+git clone --depth 1 --branch v0.0.1-pre0 https://github.com/titu1994/tfdiffeq.git
 cd tfdiffeq
 pip install .[tf-gpu]
 cd ..
